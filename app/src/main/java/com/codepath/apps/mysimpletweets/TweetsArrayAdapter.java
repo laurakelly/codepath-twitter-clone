66bddcf398e43f4aabfb91c52d335a4591803a9c
Laura Kelly <laurakelly135@gmail.com>
2016-08-17 13:32:23 -0700

    use recycler view for tweets list

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -1,44 +1,85 @@
 package com.codepath.apps.mysimpletweets;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by laura_kelly on 8/15/16.
  */
-public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
+public class TweetsArrayAdapter extends RecyclerView.Adapter<TweetsArrayAdapter.ViewHolder> {
 
   public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
-    super(context, android.R.layout.simple_list_item_1, tweets);
+    mTweets = new ArrayList<Tweet>();
+    mContext = context;
+  }
+
+  public static class ViewHolder extends RecyclerView.ViewHolder {
+    public ImageView ivProfileImage;
+    public TextView tvUserName;
+    public TextView tvBody;
+
+    public ViewHolder(View itemView) {
+      super(itemView);
+
+      ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+      tvUserName = (TextView) itemView.findViewById(R.id.tvUserName);
+      tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+    }
+  }
+
+  private List<Tweet> mTweets;
+  private Context mContext;
+
+  private Context getContext() {
+    return mContext;
   }
 
   @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    Tweet tweet = getItem(position);
-    if (convertView == null) {
-      convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
-    }
+  public TweetsArrayAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    Context context = parent.getContext();
+    LayoutInflater inflater = LayoutInflater.from(context);
 
-    ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
-    TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
-    TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+    View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+
+    ViewHolder viewHolder = new ViewHolder(tweetView);
+    return viewHolder;
+  }
+
+  @Override
+  public void onBindViewHolder(TweetsArrayAdapter.ViewHolder viewHolder, int position) {
+    Tweet tweet = mTweets.get(position);
+
+    ImageView ivProfileImage = viewHolder.ivProfileImage;
+    TextView tvUserName = viewHolder.tvUserName;
+    TextView tvBody = viewHolder.tvBody;
 
     tvUserName.setText(tweet.getUser().getScreenName());
     tvBody.setText(tweet.getBody());
+
     ivProfileImage.setImageResource(android.R.color.transparent);
     Picasso.with(getContext())
             .load(tweet.getUser().getProfileImageUrl())
             .into(ivProfileImage);
-    return convertView;
+  }
+
+  @Override
+  public int getItemCount() {
+    return mTweets.size();
+  }
+
+  public void addAll(List<Tweet> tweets) {
+    mTweets.addAll(tweets);
+    this.notifyDataSetChanged();
   }
 }
--- app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetsListFragment.java
@@ -3,10 +3,11 @@ package com.codepath.apps.mysimpletweets.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.TweetsArrayAdapter;
@@ -21,22 +22,30 @@ import java.util.List;
 public class TweetsListFragment extends Fragment {
   private ArrayList<Tweet> tweets;
   private TweetsArrayAdapter aTweets;
-  private ListView lvTweets;
-
-  @Nullable
-  @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
-    View v = inflater.inflate(R.layout.fragment_tweets_list, parent, false);
-    lvTweets = (ListView) v.findViewById(R.id.lvTweets);
-    lvTweets.setAdapter(aTweets);
-    return v;
-  }
+  private RecyclerView rvTweets;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     tweets = new ArrayList<>();
     aTweets = new TweetsArrayAdapter(getActivity(), tweets);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_tweets_list, parent, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    rvTweets = (RecyclerView) view.findViewById(R.id.rvTweets);
+    rvTweets.setAdapter(aTweets);
+
+    rvTweets.setLayoutManager(new LinearLayoutManager(getContext()));
   }
 
   public void addAll(List<Tweet> tweets) {
